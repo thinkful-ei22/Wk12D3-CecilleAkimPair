390608cb25297b21b2fa36fc4569e5f7d3aa6bba
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-29 10:50:10 -0700

    Separated out main function from Array class - still getting error

--- array.js
+++ array.js
@@ -6,59 +6,74 @@ class Array {
      this.ptr = memory.allocate(this.length);
    }
 
-   _resize(size) {
-        const oldPtr = this.ptr;
-        this.ptr = memory.allocate(size);
-        if (this.ptr === null) {
-            throw new Error('Out of memory');
-        }
-        memory.copy(this.ptr, oldPtr, this.length);
-        memory.free(oldPtr);
-    }
+     _resize(size) {
+          const oldPtr = this.ptr;
+          this.ptr = memory.allocate(size);
+          if (this.ptr === null) {
+              throw new Error('Out of memory');
+          }
+          memory.copy(this.ptr, oldPtr, this.length);
+          memory.free(oldPtr);
+      }
 
-   push(value) {
-     this._resize(this.length + 1);
-     memory.set(this.ptr + this.length, value);
-     this.length++;
-   }
+     push(value) {
+       this._resize(this.length + 1);
+       memory.set(this.ptr + this.length, value);
+       this.length++;
+     }
 
-   get(index) {
-        if (index < 0 || index >= this.length) {
-            throw new Error('Index error');
-        }
-        return memory.get(this.ptr + index);
-    }
+     get(index) {
+          if (index < 0 || index >= this.length) {
+              throw new Error('Index error');
+          }
+          return memory.get(this.ptr + index);
+      }
 
-    pop() {
-        if (this.length == 0) {
-            throw new Error('Index error');
-        }
-        const value = memory.get(this.ptr + this.length - 1);
-        this.length--;
-        return value;
-    }
+      pop() {
+          if (this.length == 0) {
+              throw new Error('Index error');
+          }
+          const value = memory.get(this.ptr + this.length - 1);
+          this.length--;
+          return value;
+      }
 
-    insert(index, value) {
-        if (index < 0 || index >= this.length) {
-            throw new Error('Index error');
-        }
+      insert(index, value) {
+          if (index < 0 || index >= this.length) {
+              throw new Error('Index error');
+          }
 
-        if (this.length >= this._capacity) {
-            this._resize((this.length + 1) * Array.SIZE_RATIO);
-        }
+          if (this.length >= this._capacity) {
+              this._resize((this.length + 1) * Array.SIZE_RATIO);
+          }
 
-        memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
-        memory.set(this.ptr + index, value);
-        this.length++;
+          memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+          memory.set(this.ptr + index, value);
+          this.length++;
+      }
 
-
-    remove(index) {
-        if (index < 0 || index >= this.length) {
-            throw new Error('Index error');
-        }
-        memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
-        this.length--;
-    }
+      remove(index) {
+          if (index < 0 || index >= this.length) {
+              throw new Error('Index error');
+          }
+          memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+          this.length--;
+      }
 }
 
 Array.SIZE_RATIO = 3;
+
+function main() {
+
+  Array.SIZE_RATIO = 3;
+
+  //create an instance of the array class
+  let arr = new Array();
+
+  //add an item to the array
+  arr.push(3);
+
+  console.log(arr);
+}
+
+main();
